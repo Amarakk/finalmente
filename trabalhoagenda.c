e8df2b8850e076f794c520bca8987229f44b178b
Amarakk <53926463+Amarakk@users.noreply.github.com>
2019-11-20 15:06:40 -0300

    Update trabalhoagenda.c

--- trabalhoagenda.c
+++ trabalhoagenda.c
@@ -17,11 +17,6 @@ agenda contatos[100];
 
 
 agenda cadastro(){
-    
-    char *ptr1=NULL;
-    char *ptr2=NULL;
-    char *ptr3=NULL;
-    agenda novo;
     char aux1[100];
     char aux2[100];
     char aux3[100];
@@ -32,9 +27,8 @@ agenda cadastro(){
     getchar();fflush(stdin);
     fgets(aux1,100,stdin);
     contatos[contadorGlobal].nome=(char*)malloc(strlen(aux1)*sizeof(char));
-    for(int i=0; i<strlen(aux1); i++){
-        contatos[contadorGlobal].nome[i]=aux1[i];
-    }
+    strcpy(contatos[contadorGlobal].nome,aux1);
+   
 
     printf("\n\n");
     printf("Digite o telefone: ");
@@ -48,9 +42,9 @@ agenda cadastro(){
     fflush(stdin);
     fgets(aux2,100,stdin);
     contatos[contadorGlobal].endereco=(char*)malloc(strlen(aux2)*sizeof(char));
-    for(int i=0; i<strlen(aux2); i++){
-        contatos[contadorGlobal].endereco[i]=aux2[i];
-    }
+
+    strcpy(contatos[contadorGlobal].endereco,aux2);
+    
     
     
 
@@ -59,9 +53,8 @@ agenda cadastro(){
     fflush(stdin);
     fgets(aux3,100,stdin);
     contatos[contadorGlobal].cidade=(char*)malloc(strlen(aux3)*sizeof(char));
-    for(int i=0; i<strlen(aux3); i++){
-        contatos[contadorGlobal].cidade[i]=aux3[i];
-    }
+    strcpy(contatos[contadorGlobal].cidade,aux3);
+    
     printf("\n\n");
     printf("Digite o estado(sigla): ");
     fflush(stdin);
@@ -78,14 +71,18 @@ agenda cadastro(){
 int pesquisar(){
     char nome[100];
     char* ptr;
-    int x;
+    char comp[100];
+    int x=1;
     int result;
     getchar();fflush(stdin);
 
     printf("\n\nDigite o nome do contato: ");
     fgets(nome,99,stdin);
-    for(int i=0; i<100;i++){
+    for(int i=0; i<contadorGlobal;i++){
+        
+        
         x = strcmp(contatos[i].nome,nome);
+
         
         if (x == 0){
             printf("\nNome: %s",contatos[i].nome);
@@ -97,6 +94,7 @@ int pesquisar(){
             result=i;
             break;
         }
+       
     }
     if (x != 0){
         printf("\nContato nao encontrado\n");
@@ -105,14 +103,71 @@ int pesquisar(){
     return result;
 }
 
-agenda modificar(){
+int modificar(){
     int indice;
     printf("Modificação de contato");
-    indice = pesquisar();
     
-    if (indice!=111){
-        contatos[indice]=cadastro();
+    for(int i=0; i<=contadorGlobal;i++){
+        printf("\n%d-%s",i,contatos[i].nome);
     }
+    printf("\nEscolha um contato\n");
+    scanf("%d",&indice);
+
+    return indice;
+
+
+}
+
+agenda modificarParte2(int change){
+
+    char aux1[100];
+    char aux2[100];
+    char aux3[100];
+
+    fflush(stdin);
+    printf("\n\n");
+    printf("Digite o nome: ");
+    getchar();fflush(stdin);
+    fgets(aux1,100,stdin);
+    contatos[change].nome=(char*)malloc(strlen(aux1)*sizeof(char));
+    strcpy(contatos[change].nome,aux1);
+   
+
+    printf("\n\n");
+    printf("Digite o telefone: ");
+    fflush(stdin);
+    fgets(contatos[change].telefone,15,stdin);
+
+  
+
+    printf("\n\n");
+    printf("Digite o endereco: ");
+    fflush(stdin);
+    fgets(aux2,100,stdin);
+    contatos[change].endereco=(char*)malloc(strlen(aux2)*sizeof(char));
+
+    strcpy(contatos[change].endereco,aux2);
+    
+    
+    
+
+    printf("\n\n");
+    printf("Digite o cidade: ");
+    fflush(stdin);
+    fgets(aux3,100,stdin);
+    contatos[change].cidade=(char*)malloc(strlen(aux3)*sizeof(char));
+    strcpy(contatos[change].cidade,aux3);
+    
+    printf("\n\n");
+    printf("Digite o estado(sigla): ");
+    fflush(stdin);
+    fgets(contatos[change].estado,4,stdin);
+   
+
+    printf("\n\n");
+    printf("Digite o dia de nascimento(dd mm aaaa separado por espaco): ");
+    
+    scanf("%d %d %d",&contatos[change].dia,&contatos[change].mes,&contatos[change].ano);
 
 }
 
@@ -138,6 +193,7 @@ void menu () {
 
 int main(){
     int action;
+    int mudanca;
     menu();
     scanf("%d",&action);
     while(action != 0) {
@@ -154,7 +210,8 @@ int main(){
             pesquisar();
         }
         if(action==3){
-            modificar();
+            mudanca=modificar();
+            modificarParte2(mudanca);
         }
         if(action==4){
             showAll();
